bc475872456fe44e64de4c35b83e82c8a0807103
Mehmet Zeytin <mzeytin@protel.com.tr>
2022-01-21 12:43:18 +0300

    Initial Commit

---
+++ MZDevTools/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MZDevTools
+//
+//  Created by Mehmet Zeytin on 21.01.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
